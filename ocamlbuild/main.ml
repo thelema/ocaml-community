fb437f1489916074b48825178f53ab358009aaf5
ertai <ertai>
2007-11-22 18:30:40 +0000

    [ocamlbuild] Give Main.Exit_codes its own file.

--- ocamlbuild/main.ml
+++ ocamlbuild/main.ml
@@ -9,7 +9,7 @@
 (*                                                                     *)
 (***********************************************************************)
 
-(* $Id: main.ml,v 1.8.2.5 2007-11-21 18:29:37 ertai Exp $ *)
+(* $Id$ *)
 (* Original author: Berke Durak *)
 open My_std
 open Log
@@ -208,25 +208,6 @@ let proceed () =
         (Exit_build_error
           (sbprintf "@[<2>Circular dependencies: %S already seen in@ %a@]@." p pp_l seen))
 ;;
-
-module Exit_codes =
-  struct
-    let rc_ok                  = 0
-    let rc_usage               = 1
-    let rc_failure             = 2
-    let rc_invalid_argument    = 3
-    let rc_system_error        = 4
-    let rc_hygiene             = 1
-    let rc_circularity         = 5
-    let rc_solver_failed       = 6
-    let rc_ocamldep_error      = 7
-    let rc_lexing_error        = 8
-    let rc_build_error         = 9
-    let rc_executor_reserved_1 = 10 (* Redefined in Executor *)
-    let rc_executor_reserved_2 = 11
-    let rc_executor_reserved_3 = 12
-    let rc_executor_reserved_4 = 13
-  end
 
 open Exit_codes;;
 
